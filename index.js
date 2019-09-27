315b6d20d35ced38bfa7f1973a5bd192ad30fa35
Roy Riojas <royriojas@gmail.com>
2019-09-27 03:22:04 -0500

    FIX: Add option to process the tags before generating the log file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = (opts, reporterOpts) => {
   const cLog = require('./lib/changelog');
   const options = {
-    tagPrefix: opts.tagPrefix,
-    tagRange: opts.tagRange,
+    tagPrefix: opts.tagPrefix || reporterOpts.tagPrefix,
+    tagRange: opts.tagRange || reporterOpts.tagRange,
     // use the value from the config or from the cli interface. Cli interface has
     // preference over the config one
     ignoreRegExp: opts.ignoreRegExp || reporterOpts.ignoreRegExp,
     processCommit: reporterOpts.processCommit,
+    processTags: reporterOpts.processTags,
   };
 
   const p = cLog.getCommits(options);
